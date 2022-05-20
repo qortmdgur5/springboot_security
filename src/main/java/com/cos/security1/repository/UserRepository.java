ae974b3a583d67c1e6d8a61d14e2878353e18b4e
bsh <dnfg12@naver.com>
2022-05-20 19:20:11 +0900

    spring security 로그인 기능 구현

--- src/main/java/com/cos/security1/repository/UserRepository.java
+++ src/main/java/com/cos/security1/repository/UserRepository.java
@@ -8,5 +8,7 @@ import com.cos.security1.model.User;
 //  CRUD 함수를 JapRepository가 들고 있다.
 //	 @Repository라는 어노테이션이 없어도 IoC됨. 이유는 JpaRepository를 상속했기 때문
 public interface UserRepository extends JpaRepository<User, Integer>{
-
+	
+	public User findByUsername(String username);	//  Jpa Query methods
+	
 }
